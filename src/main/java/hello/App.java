2eac9a3c3b1f93bfca3c4bb4e127dcb6187d7044
ozaki25 <fs65ugumori@yahoo.co.jp>
2016-06-15 10:44:07 +0900

    RestControllerに変えた

--- src/main/java/hello/App.java
+++ src/main/java/hello/App.java
@@ -1,15 +1,14 @@
 package hello;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @EnableAutoConfiguration
 public class App {
     @RequestMapping("/")
-    @ResponseBody
     public String home() {
         return "Hello World!";
     }
