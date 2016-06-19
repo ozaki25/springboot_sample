f173175ac168b80b1deea9786fe517517e4c0cb6
ozaki25 <fs65ugumori@yahoo.co.jp>
2016-06-20 08:03:35 +0900

    crossoriginの設定を追加

--- src/main/java/sample/UsersController.java
+++ src/main/java/sample/UsersController.java
@@ -1,6 +1,7 @@
 package sample;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3010")
 @RestController
 @RequestMapping("/users")
 public class UsersController {
