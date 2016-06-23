842a4df8196b2521e62dd88c962dab41309530c6
ozaki25 <fs65ugumori@yahoo.co.jp>
2016-06-23 17:39:52 +0900

    不要なコードを削除

--- src/main/java/com/example/workflow/App.java
+++ src/main/java/com/example/workflow/App.java
@@ -1,9 +1,7 @@
 package com.example.workflow;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
