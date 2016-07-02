0aa8bdef664821bfc95c1db5442d3a6489e9ed3c
ozaki <fs65ugumori@yahoo.co.jp>
2016-07-02 23:02:31 +0900

    ユーザログイン対応

--- src/main/java/com/example/workflow/App.java
+++ src/main/java/com/example/workflow/App.java
@@ -1,15 +1,24 @@
 package com.example.workflow;
 
+import java.security.Principal;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @Controller
 public class App {
+    @Autowired
+    private UserRepository userRepository;
+
     @RequestMapping("/")
-    public String home() {
+    public String home(Model model, Principal principal) {
+        User user = new User();
+        if(principal != null) user = userRepository.findByUid(principal.getName());
+        model.addAttribute("user", user);
         return "index";
     }
 
