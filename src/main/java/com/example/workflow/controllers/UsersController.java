c964de96869312aa6680bfcb732f704e2bd65097
ozaki25 <fs65ugumori@yahoo.co.jp>
2016-07-07 19:14:07 +0900

    ユーザ一覧を検索できるようにした

--- src/main/java/com/example/workflow/controllers/UsersController.java
+++ src/main/java/com/example/workflow/controllers/UsersController.java
@@ -3,12 +3,16 @@ package com.example.workflow;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
@@ -18,8 +22,28 @@ public class UsersController {
     private UserRepository repository;
 
     @RequestMapping(method = RequestMethod.GET)
-    public List<User> index() {
-        List<User> users = repository.findAll();
+    public List<User> index(@RequestParam MultiValueMap<String, String> query) {
+        logger.info("query: " + query);
+        List<String> teams = query.get("team");
+        String team = teams == null ? null : teams.get(0);
+        List<String> jobLevelsString = query.get("jobLevel[]");
+        List<Integer> jobLevels = new ArrayList<Integer>();
+        if(jobLevelsString != null) for(String jobLevel : jobLevelsString) jobLevels.add(Integer.parseInt(jobLevel));
+
+        List<User> users = new ArrayList<User>();
+        if(team != null && jobLevels.size() != 0) {
+            logger.info("repository.findByTeamAndJobLevelIn");
+            users = repository.findByTeamAndJobLevelIn(team, jobLevels);
+        } else if(team != null) {
+            logger.info("repository.findByTeam");
+            users = repository.findByTeam(team);
+        } else if(jobLevels.size() != 0) {
+            logger.info("repository.findByJobLevelIn");
+            users = repository.findByJobLevelIn(jobLevels);
+        } else {
+            logger.info("repository.findAll");
+            users = repository.findAll();
+        }
         logger.info(User.toString(users));
         return users;
     }
--- src/main/java/com/example/workflow/repositories/UserRepository.java
+++ src/main/java/com/example/workflow/repositories/UserRepository.java
@@ -6,5 +6,8 @@ import java.util.List;
 public interface UserRepository extends JpaRepository<User, Long> {
     User findById(Long id);
     User findByUid(String uid);
+    List<User> findByTeam(String team);
+    List<User> findByJobLevelIn(List<Integer> jobLevels);
+    List<User> findByTeamAndJobLevelIn(String team, List<Integer> jobLevels);
     List<User> findAll();
 }
